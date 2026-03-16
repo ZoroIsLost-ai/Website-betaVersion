fc8b3c26a2bca0701d811f0dd348b7dfe23f05a9
Ayush Anand <anandayush983@gmail.com>
2026-03-16 18:38:11 +0530

    just some changes in the top slide and the picture part

--- js/script.js
+++ js/script.js
@@ -8,3 +8,33 @@ document.getElementById("content").style.display = "block";
 },1000);
 
 });
+let currentSize = 16;
+
+function changeFontSize(action){
+
+if(action === -1){
+currentSize -= 1;
+}
+
+if(action === 1){
+currentSize += 1;
+}
+
+if(action === 0){
+currentSize = 16;
+}
+
+document.body.style.fontSize = currentSize + "px";
+
+}
+
+function translatePage(lang){
+
+let select = document.querySelector(".goog-te-combo");
+
+if(select){
+select.value = lang;
+select.dispatchEvent(new Event("change"));
+}
+
+}
